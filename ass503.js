6d20de72d974b9ecebd39ac49da0c82fc50d0145
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2021-12-30 14:39:57 +0530

    Add files via upload

---
+++ ass503.js
@@ -0,0 +1,18 @@
+//check whether it is perfect square or not
+    
+var num =parseInt(prompt('Enter the number to check perfect square or not'));
+var squareroot = Math.sqrt(num);
+var i= Math.floor(squareroot);
+if(i*i== num)
+{
+ alert("Number is a perfect square");
+}
+else
+{
+alert("Number is not a perfect square");
+}
+                
+            
+  
+ 
+           
